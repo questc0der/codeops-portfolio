5bbb08331e279c19e465f49aa08da7bb55ff1f31
questc0der <pour64moi@gmail.com>
2026-08-11 10:06:44 +0300

    add order.js with subtotal, discount, VAT, and receipt maker functions

---
+++ Module-02/day07/class/order.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * Write subtotal(...prices) using a reduce callback.
+ * Use rest parameters to accept any number of prices.
+ */
+const subtotal = (...prices) => prices.reduce((acc, curr) => acc + curr, 0);
+
+/**
+ * Write discountBy(rate) as a factory returning an arrow function.
+ * This is a Higher-Order Function (HOF) that creates a closure over the rate.
+ */
+const discountBy = (rate) => (amount) => amount * (1 - rate);
+
+/**
+ * Add withVat as a small pure helper.
+ * It should add 15% VAT to a given amount.
+ */
+const withVat = (n) => n * 1.15;
+
+/**
+ * Add toETB as a small pure helper.
+ * It should format a number to 2 decimal places followed by " ETB".
+ */
+const toETB = (n) => {};
+
+/**
+ * Build makeReceiptMaker() with a private order number.
+ * This function uses a closure to maintain the state of orderNo across calls.
+ * Inside, it should pre-build a 10% member discount function using discountBy(0.10).
+ */
+function makeReceiptMaker() {
+  return function () {};
+}
